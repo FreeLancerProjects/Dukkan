279fbfa94a5e9981dc85aca931a8e0ae796a6a75
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2019-01-08 14:14:33 +0200

    emad

---
+++ app/src/main/java/com/appzone/dukkan/activities_fragments/home_activity/driver_home/DriverHomeActivity.java
@@ -0,0 +1,15 @@
+package com.appzone.dukkan.activities_fragments.home_activity.driver_home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appzone.dukkan.R;
+
+public class DriverHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_home);
+    }
+}
